another bugfix to delay calculation

pypulseq/make_block_pulse.py
@@ -113,7 +113,9 @@ def make_block_pulse(flip_angle: float, bandwidth: float = 0, delay: float = 0, 
 
         if rf.delay < (gz.rise_time + gz.delay):
             rf.delay = gz.rise_time + gz.delay
-
+    else:
+        gz = None
+        
     if rf.ringdown_time > 0:
         t_fill = np.arange(1, round(rf.ringdown_time / 1e-6) + 1) * 1e-6
         rf.t = np.concatenate((rf.t, (rf.t[-1] + t_fill)))