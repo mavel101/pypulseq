fix bug in delay calculation

pypulseq/make_arbitrary_rf.py
@@ -127,7 +127,7 @@ def make_arbitrary_rf(signal: np.ndarray, flip_angle: float, bandwidth: float = 
     if return_delay:
         # create a delay object to avoid zero filling after RF pulse
         if gz is not None:
-            delay = gz.rise_time + gz.flat_time + gz.fall_time
+            delay = gz.delay + gz.rise_time + gz.flat_time + gz.fall_time
             if rf.ringdown_time > gz.fall_time:
                 delay += rf.ringdown_time - gz.fall_time
 