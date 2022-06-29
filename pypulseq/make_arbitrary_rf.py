fix delay calculation

pypulseq/make_arbitrary_rf.py
@@ -130,11 +130,10 @@ def make_arbitrary_rf(signal: np.ndarray, flip_angle: float, bandwidth: float = 
             delay = gz.delay + gz.rise_time + gz.flat_time + gz.fall_time
             if rf.ringdown_time > gz.fall_time:
                 delay += rf.ringdown_time - gz.fall_time
-
-            rf_delay = make_delay(d=delay)
         else:
             delay = rf.delay + rf.t[-1] + rf.ringdown_time
-            rf_delay = make_delay(d=delay)
+        
+        rf_delay = make_delay(d=math.ceil(delay / system.grad_raster_time) * system.grad_raster_time)
     
     if return_delay and return_gz:
         return rf, gz, rf_delay

pypulseq/make_sigpy_pulse.py
@@ -159,11 +159,10 @@ def sigpy_n_seq(
             delay = gz.delay + gz.rise_time + gz.flat_time + gz.fall_time
             if rfp.ringdown_time > gz.fall_time:
                 delay += rf.ringdown_time - gz.fall_time
-
-            rf_delay = make_delay(d=delay)
         else:
             delay = rfp.delay + rfp.t[-1] + rfp.ringdown_time
-            rf_delay = make_delay(d=delay)
+        
+        rf_delay = make_delay(d=math.ceil(delay / system.grad_raster_time) * system.grad_raster_time)
 
     # Following 2 lines of code are workarounds for numpy returning 3.14... for np.angle(-0.00...)
     negative_zero_indices = np.where(rfp.signal == -0.0)